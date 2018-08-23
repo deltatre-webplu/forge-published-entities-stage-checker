ae1a1e6e0bd4d7b93ff07fa7fa3d8cbec2d9d451
Enrico Massone <enrico.massone@deltatre.it>
2018-08-23 12:49:05 +0200

    Implemented a class to validate the provided configuration

---
+++ ForgePublishedEntitiesStageChecker/ForgePublishedEntitiesStageChecker/ConfigurationValidator.cs
@@ -0,0 +1,39 @@
+﻿using Deltatre.Utils.Dto;
+using Deltatre.Utils.Extensions.Enumerable;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace ForgePublishedEntitiesStageChecker
+{
+	public class ConfigurationValidator
+	{
+		private const string MongoConnStringConfigKey = "MongoConnString";
+		private const string LogFilePathConfigKey = "LogFilePath";
+		private const string ReportFilePathConfigKey = "ReportFilePath";
+
+		public ValidationResult<IConfiguration, string> Validate(IConfiguration configuration)
+		{
+			if (configuration == null)
+				throw new ArgumentNullException(nameof(configuration));
+
+			var missingConfigurations = new List<string>();
+
+			if (string.IsNullOrWhiteSpace(configuration[MongoConnStringConfigKey]))
+				missingConfigurations.Add(MongoConnStringConfigKey);
+
+			if (string.IsNullOrWhiteSpace(configuration[LogFilePathConfigKey]))
+				missingConfigurations.Add(LogFilePathConfigKey);
+
+			if (string.IsNullOrWhiteSpace(configuration[ReportFilePathConfigKey]))
+				missingConfigurations.Add(ReportFilePathConfigKey);
+
+			if (missingConfigurations.Count == 0)
+				return ValidationResult<IConfiguration, string>.CreateValid(configuration);
+
+			var errorMessage = $"Please provide following configuration(s): {string.Join(" ", missingConfigurations)}";
+			var errors = new[] { errorMessage }.ToNonEmptySequence();
+			return ValidationResult<IConfiguration, string>.CreateInvalid(errors);
+		}
+	}
+}
--- ForgePublishedEntitiesStageChecker/ForgePublishedEntitiesStageChecker/Program.cs
+++ ForgePublishedEntitiesStageChecker/ForgePublishedEntitiesStageChecker/Program.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace ForgePublishedEntitiesStageChecker
 {
-	public class Program
+	public static class Program
 	{
 		public static void Main(string[] args)
 		{
@@ -13,7 +13,7 @@ namespace ForgePublishedEntitiesStageChecker
 			.AddCommandLine(args)
 			.Build();
 
-			Console.WriteLine(config["Message"]);
+			Console.WriteLine(config["Foo"]);
 			Console.ReadLine();
 		}
 	}
