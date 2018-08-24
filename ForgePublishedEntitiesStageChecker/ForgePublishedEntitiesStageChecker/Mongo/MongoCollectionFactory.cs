7d1af7e5c00dd99aaebdf86623ef0bd78339f660
Enrico Massone <enrico.massone@deltatre.it>
2018-08-24 10:59:44 +0200

    introduced factory class for mongo collections

---
+++ ForgePublishedEntitiesStageChecker/ForgePublishedEntitiesStageChecker/Mongo/MongoCollectionFactory.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace ForgePublishedEntitiesStageChecker.Mongo
+{
+	public class MongoCollectionFactory
+	{
+		private readonly Lazy<IMongoDatabase> _backendDatabase;
+
+		public MongoCollectionFactory(string connString)
+		{
+			if (string.IsNullOrWhiteSpace(connString))
+				throw new ArgumentException("Backend store connection string cannot be null or white space.", nameof(connString));
+
+			_backendDatabase = new Lazy<IMongoDatabase>(() => GetDatabaseFromConnString(connString));
+		}
+
+		public IMongoCollection<BsonDocument> GetMongoCollection(string collectionName)
+		{
+			return _backendDatabase.Value.GetCollection<BsonDocument>(
+				collectionName,
+				new MongoCollectionSettings
+				{
+					GuidRepresentation = GuidRepresentation.CSharpLegacy
+				});
+		}
+
+		private static IMongoDatabase GetDatabaseFromConnString(string connString)
+		{
+			var mongoUrl = MongoUrl.Create(connString);
+			var databaseName = mongoUrl.DatabaseName;
+
+			var mongoClient = new MongoClient(connString);
+			var database = mongoClient.GetDatabase(databaseName);
+			return database;
+		}
+	}
+}
