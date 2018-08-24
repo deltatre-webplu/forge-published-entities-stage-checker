a3f47693a9c99ff3f0069a624bb34c4582de3908
Enrico Massone <enrico.massone@deltatre.it>
2018-08-24 10:46:01 +0200

    Added class to extract settings

---
+++ ForgePublishedEntitiesStageChecker/ForgePublishedEntitiesStageChecker/Configuration/Settings.cs
@@ -0,0 +1,16 @@
+﻿namespace ForgePublishedEntitiesStageChecker.Configuration
+{
+	public class Settings
+	{
+		public string MongoConnString { get; }
+		public string LogFilePath { get; }
+		public string ReportFilePath { get; }
+
+		public Settings(string mongoConnString, string logFilePath, string reportFilePath)
+		{
+			this.MongoConnString = mongoConnString;
+			this.LogFilePath = logFilePath;
+			this.ReportFilePath = reportFilePath;
+		}
+	}
+}
