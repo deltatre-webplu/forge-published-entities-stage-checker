f033fd320d793119d8483aa0d3c6f55934b0cf88
Enrico Massone <enrico.massone@deltatre.it>
2018-08-25 12:14:39 +0200

    Added class tenants configuration reader

---
+++ ForgePublishedEntitiesStageChecker/ForgePublishedEntitiesStageChecker/Configuration/TenantConfigurationReader.cs
@@ -0,0 +1,23 @@
+﻿using ForgePublishedEntitiesStageChecker.Contracts;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+
+namespace ForgePublishedEntitiesStageChecker.Configuration
+{
+	public class TenantConfigurationReader
+	{
+		public ReadOnlyCollection<Tenant> ReadTenantsFromFile(string tenantFilePath)
+		{
+			using (var streamReader = File.OpenText(tenantFilePath))
+			{
+				var serializer = new JsonSerializer();
+				var tenants = (IEnumerable<Tenant>)serializer.Deserialize(streamReader, typeof(IEnumerable<Tenant>));
+
+				return new ReadOnlyCollection<Tenant>(tenants.ToList());
+			}
+		}
+	}
+}
---
+++ ForgePublishedEntitiesStageChecker/ForgePublishedEntitiesStageChecker/Contracts/Tenant.cs
@@ -0,0 +1,8 @@
+﻿namespace ForgePublishedEntitiesStageChecker.Contracts
+{
+	public class Tenant
+	{
+		public string Name { get; set; }
+		public string ConnString { get; set; }
+	}
+}
